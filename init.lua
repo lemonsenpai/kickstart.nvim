0d9d2887a9f1772c5f048204364e4e92d0e93ba5
mdeleon <mrvndeleon0@gmail.com>
2024-09-08 22:03:45 -0500

    fix warnings

--- init.lua
+++ init.lua
@@ -43,6 +43,8 @@ require('lazy').setup({
   -- Detect tabstop and shiftwidth automatically
   'tpope/vim-sleuth',
 
+  'tpope/vim-surround',
+
   -- Make neovim transparent to show terminal background image
   'xiyaowong/transparent.nvim',
 
@@ -81,8 +83,35 @@ require('lazy').setup({
     },
   },
 
+
   -- Useful plugin to show you pending keybinds.
-  { 'folke/which-key.nvim',  opts = {} },
+  {
+    'folke/which-key.nvim',
+    event = 'VimEnter',
+    opts = {
+      icons = {
+        mappings = vim.g.have_nerd_font
+      }
+    },
+    dependencies = { 'echasnovski/mini.nvim', 'nvim-tree/nvim-web-devicons' },
+
+    keys = {
+      { '<leader>c',  group = '[C]ode' },
+      { '<leader>c_', hidden = true },
+      { '<leader>d',  group = '[D]ocument' },
+      { '<leader>d_', hidden = true },
+      { '<leader>g',  group = '[G]it' },
+      { '<leader>g_', hidden = true },
+      { '<leader>h',  group = 'More git' },
+      { '<leader>h_', hidden = true },
+      { '<leader>r',  group = '[R]ename' },
+      { '<leader>r_', hidden = true },
+      { '<leader>s',  group = '[S]earch' },
+      { '<leader>s_', hidden = true },
+      { '<leader>w',  group = '[W]orkspace' },
+      { '<leader>w_', hidden = true },
+    }
+  },
 
   -- -- file explorer . just like vscode!
   -- {
@@ -466,15 +495,6 @@ local on_attach = function(_, bufnr)
 end
 
 -- document existing key chains
-require('which-key').register({
-  ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-  ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-  ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
-  ['<leader>h'] = { name = 'More git', _ = 'which_key_ignore' },
-  ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-  ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-  ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-})
 
 -- Enable the following language servers
 --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
@@ -489,8 +509,8 @@ local servers = {
   -- gopls = {},
   -- pyright = {},
   -- rust_analyzer = {},
-  -- tsserver = {},
-  -- html = { filetypes = { 'html', 'twig', 'hbs'} },
+  ts_ls = {},
+  html = { filetypes = { 'html', 'twig', 'hbs' } },
 
   lua_ls = {
     Lua = {
