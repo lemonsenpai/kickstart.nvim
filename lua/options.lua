adc5e1ab8d3c47c9a7dbcb94b81eacd9108b3dba
mdeleon <mrvndeleon0@gmail.com>
2024-09-12 23:01:40 -0500

    tried to update tabs and stuff

--- lua/options.lua
+++ lua/options.lua
@@ -61,3 +61,8 @@ vim.opt.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
+
+vim.opt.tabstop = 4 -- A TAB character looks like 4 spaces
+vim.opt.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
+vim.opt.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
+vim.opt.shiftwidth = 4 -- Number of spaces inserted when indenting
