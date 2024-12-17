bd36ac692f83d8e145891187ca9b908f49e2ac0c
mdeleon <mrvndeleon0@gmail.com>
2024-12-16 23:59:38 -0600

    new stuff :)

--- init.lua
+++ init.lua
@@ -415,7 +415,8 @@ require('lazy').setup({
         -- python = { "isort", "black" },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
-        -- javascript = { "prettierd", "prettier", stop_after_first = true },
+        javascript = { 'prettier', stop_after_first = true },
+        typescript = { 'prettier', stop_after_first = true },
       },
     },
   },
